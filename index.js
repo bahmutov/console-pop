768172b488b07c71a93899e591816da7cfc829bf
Gleb Bahmutov <gleb@kensho.com>
2014-02-25 22:23:55 -0500

    hiding console output

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 'use strict';
 
-var stack = {
-  push: function () {
+var buffered = {
+  log: []
+};
 
+var _log;
+
+var stack = {
+  push: function (options) {
+    options = options || {};
+    options.silent = options.silent || options.quiet;
+
+    _log = console.log;
+    buffered.log = [];
+
+    console.log = function () {
+      var args = [].slice.call(arguments);
+      var msg = args.join('');
+      buffered.log.push(msg);
+      if (!options.silent) {
+        _log.apply(null, args);
+      }
+    };
   },
   pop: function () {
-    return '';
+    var str = buffered.log.join('\n');
+    console.log = _log;
+    return str;
   }
 };
 
--- test/test.js
+++ test/test.js
@@ -3,14 +3,44 @@
 
 var assert = require('assert');
 
-describe('console-pop', function() {
+describe('console-pop', function () {
   var stack = require('..');
-  describe('exists', function (){
-    it('should operate on console', function() {
+  describe('exists', function () {
+    it('should operate on console', function () {
       stack.push();
       console.log('working');
       var str = stack.pop();
       assert.equal('working', str, 'captured string');
+    });
+
+    it('can be used several times', function () {
+      stack.push();
+      console.log('one');
+      var str = stack.pop();
+      assert(/one/.test(str), 'has first string');
+
+      stack.push();
+      console.log('two');
+      str = stack.pop();
+      assert(!/one/.test(str), 'does not have first string');
+      assert(/two/.test(str), 'has second string');
+    });
+
+    it('can hide output', function () {
+      var _log = console.log;
+      console.log = function () {
+        console.log = _log;
+        assert(false, 'called console.log!');
+      };
+
+      stack.push({
+        silent: true
+      });
+      console.log('one');
+      var str = stack.pop();
+      console.log = _log;
+
+      assert(/one/.test(str), 'console.log not called');
     });
   });
 });
